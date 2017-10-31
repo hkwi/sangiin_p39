1feb6f6f44ad1796ab856bc15d84f93110cdbc05
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2017-10-31 23:52:26 +0900

    rename and drop exec prop

--- tests/test_job.py
+++ tests/test_job.py
@@ -1,8 +1,8 @@
 import sangiin_p39.wayback
 import sangiin_p39.wikidata
 
-def test_wayback:
+def test_wayback():
 	sangiin_p39.wayback.run()
 
-def test_wikidata:
+def test_wikidata():
 	sangiin_p39.wikidata.run()
